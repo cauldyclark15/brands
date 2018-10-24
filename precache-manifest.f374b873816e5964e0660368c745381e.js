4c71f5c1456941d1996b6f7e69c0547f5204b0c9
cauldyclark15 <jcing.jc@gmail.com>
2018-10-25 00:13:53 +0800

    Updates

---
+++ precache-manifest.f374b873816e5964e0660368c745381e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/brands/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/brands/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/brands/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "3bbfb950e92e783e245649e8b37d18bf",
+    "url": "/brands/static/media/login_bg.3bbfb950.jpg"
+  },
+  {
+    "revision": "f2f406691145dadad9dd",
+    "url": "/brands/static/js/runtime~main.f2f40669.js"
+  },
+  {
+    "revision": "30c9496903d8179c2ae5",
+    "url": "/brands/static/js/main.30c94969.chunk.js"
+  },
+  {
+    "revision": "b05d0f0e05dd8b533e23",
+    "url": "/brands/static/js/1.b05d0f0e.chunk.js"
+  },
+  {
+    "revision": "30c9496903d8179c2ae5",
+    "url": "/brands/static/css/main.98e55783.chunk.css"
+  },
+  {
+    "revision": "b05d0f0e05dd8b533e23",
+    "url": "/brands/static/css/1.a03ebc22.chunk.css"
+  },
+  {
+    "revision": "1f8183ed1afb0be850daebab6be30607",
+    "url": "/brands/index.html"
+  }
+];
